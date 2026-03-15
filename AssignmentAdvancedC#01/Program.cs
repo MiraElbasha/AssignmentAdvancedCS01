13a54d83b1cfd56faf6b2f9c2753bfa7620e39e7
Mira Elbasha <Mira2malak@gmail.com>
2026-03-15 20:23:50 +0200

    Q1

--- AssignmentAdvancedC#01/Program.cs
+++ AssignmentAdvancedC#01/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Generic class is a blueprint that use type parameter <T> to allow one declaration and multiple implementation
+            //why to use generics 
+            /* 1. Code reusability: we can use the same code for different data types without rewriting it
+               2. Type safety: it provides compile-time type checking, which helps to prevent runtime errors
+               3. Performance: it decreare the need for boxing and unboxing when using value types which can improve performance
+            */
+            #endregion
         }
     }
 }
